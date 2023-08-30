a55fcce8ac7b565b76c722e788ae13e23cfcb7a2
weirandrewr <78837235+weirandrewr@users.noreply.github.com>
2023-08-30 15:31:44 -0500

    Revert "remove debugger"
    This reverts commit 51b9042dcedbcbecabb33f1de5ee3f41c18b66ba.

--- config_example.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "ION_AUTH_SERVER": "auth-staging-aus.buildwithion.com",
-    "ION_API_URI": "https://api-staging-aus.buildwithion.com",
-    "ION_CLIENT_ID": "b59d6f43-2bb8-417b-9d4c-0dd6f4664fc0",
-    "ION_CLIENT_SECRET": "86118b22-2630-4b7e-bcc9-c396a2cdf737",
-}
