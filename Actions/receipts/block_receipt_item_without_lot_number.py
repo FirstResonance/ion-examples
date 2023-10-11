21ed5123a68ad72c9a4b6a91ec4487d611021350
mattemerzian <matt@firstresonance.io>
2023-10-11 14:35:15 -0400

    Add rules for blocking receipt item creation based on tracking type conditions

---
+++ Actions/receipts/block_receipt_item_without_lot_number.py
@@ -0,0 +1,15 @@
+"""
+This rule will block receiving an item if the inventory does not have a lot number.
+And the part is lot tracked.
+"""
+
+input = {
+  "enabled": true,
+  "title": "Receiving lot-tracked parts requires that a lot number is populated",
+  "target": "RECEIPTITEM",
+  "eventType": "CREATE",
+  "ruleType": "VALIDATION",
+  "errorState": "ALLOW",
+  "context": "{ receiptItem(id: $id) { id partInventory {serialNumber lotNumber part{id trackingType}} } },
+  "if not context.get('receiptItem', {}).get('partInventory', {}).get('lotNumber') and context.get('receiptItem', {}).get('partInventory', {}).get('part', {}).get('trackingType') == 'LOT': raise ValidationError()"
+}
---
+++ Actions/receipts/block_receipt_item_without_serial_number.py
@@ -0,0 +1,15 @@
+"""
+This rule will block receiving an item if the inventory does not have a serial number.
+And the part is serial tracked.
+"""
+
+input = {
+  "enabled": true,
+  "title": "Receiving serial-tracked parts requires that a serial number is populated",
+  "target": "RECEIPTITEM",
+  "eventType": "CREATE",
+  "ruleType": "VALIDATION",
+  "errorState": "ALLOW",
+  "context": "{ receiptItem(id: $id) { id partInventory {serialNumber lotNumber part{id trackingType}} } },
+  "if not context.get('receiptItem', {}).get('partInventory', {}).get('serialNumber') and context.get('receiptItem', {}).get('partInventory', {}).get('part', {}).get('trackingType') == 'SERIAL': raise ValidationError()"
+}
