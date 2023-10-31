5dcd78f1be7512b0798b1ca2e6775cc93e2029e4
mattemerzian <matt@firstresonance.io>
2023-10-31 11:33:47 -0400

    Add rule for purchase line

---
+++ Actions/purchases/block_purchase_line_if_no_need_date.py
@@ -0,0 +1,12 @@
+"""This rule will block adding a purchase line item if there is not need date."""
+
+input = {
+  "enabled": true,
+  "title": "Purchase line items must have a need date",
+  "target": "PURCHASEORDERLINE",
+  "eventType": "UPDATE",
+  "ruleType": "VALIDATION",
+  "errorState": "ALLOW",
+  "context": "{ purchaseOrderLine(id: $id) { id needDate } }",
+  "code": "if context.get('changes', {}).get('purchaseOrderLines', {}).get('status', {}).get('new') == 'ordered' and context.get('purchaseOrderLine', {}).get('needDate') is None: raise ValidationError()"
+}
